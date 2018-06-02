4515d21ee52e540442291249113752a900e8cd75
carstengrimm <carsten@carstengrimm.com>
2018-06-02 13:18:24 +0200

    initial translation, obviously not complete yet.

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Das Passwort muss mindestens 6 Zeichen enthalten.', // Passwords must be at least six characters and match the confirmation.
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben Ihnen einen Link zum Zurücksetzen Ihres Passworts gesendet!',
+    'token' => 'Der Zurücksetzungsschlüssel ist nicht gültig!',
+    'user' => "Wir können keinen Benutzer mit dieser E-Mail Adresse finden.",
+
+];
